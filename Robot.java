be61cf9738a6f34b23174db4e02a1cd73aaf79f6
Kent WU <twer@Kent-Wu-2.local>
2014-11-09 01:03:03 +0800

    kent - delete some comment and useless variables

--- Robot.java
+++ Robot.java
@@ -3,16 +3,13 @@ package robot;
 public class Robot {
 	public static int X;
 	public static int Y;
-	//position
 	public int x;
 	public int y;
-	//direction
 	public char dir;
 	
 	public String control(String order){
 		char[] orders=order.toCharArray();
 		for(char tmp : orders){
-			//×ª»»Îª´óÐ´×ÖÄ¸
 			tmp=Character.toUpperCase(tmp);
 			if(tmp == 'L' || tmp == 'R'){
 				this.setDir(tmp);
@@ -23,7 +20,6 @@ public class Robot {
 				}
 			}
 		}
-		//for end
 		return this.x+" "+this.y+" "+this.dir;
 	}
 
@@ -52,7 +48,6 @@ public class Robot {
 	}
 	
 	private void move(){
-		//NSWE
 		if(this.dir=='N'){
 			this.y+=1;
 		}else if(this.dir=='S'){
